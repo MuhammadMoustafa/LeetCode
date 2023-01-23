6f9fce743949f99564a223c7935336a543dd11d2
Muhammad Moustafa <engmuhammadmoustafa@gmail.com>
2023-01-23 16:11:01 +0200

    Time: 77 ms (38.70%), Space: 14 MB (22.53%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,19 +1,4 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:        
-        words.sort()
-        m = Counter(words)
-        h = []
-        for key, val in m.items():
-            heappush(h, (val, key))
-            
-        def keySort(ele):
-            key1 = ele[0]
-            key2 = []
-            for i in range(10):
-                r = -ord(ele[1][i]) if i < len(ele[1]) else inf
-                key2.append(r)
-            return (key1, key2)
-        
-        print("result", nlargest(k, h, key=keySort))
-        keys, values = zip(*nlargest(k, h, key=keySort))
-        return values
+        pairs = sorted(Counter(words).items(), key=lambda a: (-a[1], a[0]))
+        return [word for word, _ in pairs[0: k]]
