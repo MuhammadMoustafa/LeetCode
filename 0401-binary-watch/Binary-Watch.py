76a09c76ce94094275ee81444ffecafbde6b2085
Muhammad Moustafa <engmuhammadmoustafa@gmail.com>
2026-02-16 20:36:58 -0500

    [401] [Time Beats: 28.29%] [Memory Beats: 60.29%] - LeetPush

--- 0401-binary-watch/Binary-Watch.py
+++ 0401-binary-watch/Binary-Watch.py
@@ -1,15 +1,10 @@
 1class Solution:
 2    def readBinaryWatch(self, turnedOn: int) -> List[str]:
 3        
-4        all_possible_combinations = {}
+4        ans = []
 5        for hour in range(12):
 6            for minute in range(60):
-7                hour_leds = len([i for i in bin(hour) if i == '1'])
-8                minute_leds = len([i for i in bin(minute) if i == '1'])
-9                total_leds = hour_leds + minute_leds
-10                if total_leds in all_possible_combinations:
-11                    all_possible_combinations[total_leds].append(f"{hour}:{minute:02d}")
-12                else:
-13                    all_possible_combinations[total_leds] = [f"{hour}:{minute:02d}"]
-14        
-15        return all_possible_combinations.get(turnedOn, [])
+7                if bin(hour).count('1') + bin(minute).count('1') == turnedOn:
+8                    ans.append(f"{hour}:{minute:02d}")
+9        
+10        return ans
