1f1e97ffcb71f50e91edc1b72084d9bcf3d71ef6
Muhammad Moustafa <engmuhammadmoustafa@gmail.com>
2026-03-04 19:22:58 -0500

    [1758] [Time Beats: 83.47%] [Memory Beats: 56.18%] - LeetPush

--- 1758-minimum-changes-to-make-alternating-binary-string/Minimum-Changes-To-Make-Alternating-Binary-String.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/Minimum-Changes-To-Make-Alternating-Binary-String.py
@@ -1,24 +1,10 @@
 1class Solution:
 2    def minOperations(self, s: str) -> int:
 3        n = len(s)
-4        s1 = "0"
-5        s2 = "1"
-6
-7        def toggle_char(c):
-8            return '0' if c == '1' else '1'
-9
-10        for _ in range(1, n):
-11            s1 += toggle_char(s1[-1])
-12            s2 += toggle_char(s2[-1])
-13
-14        r1 = 0
-15        r2 = 0
-16
-17        for i in range(n):
-18            if s[i] != s1[i]:
-19                r1 += 1
-20            if s[i] != s2[i]:
-21                r2 += 1
-22
-23        return min(r1, r2)
-24
+4        diff = 0
+5        for i in range(len(s)):
+6            if s[i] != ('0' if i % 2 == 0 else '1'):
+7                diff += 1
+8
+9        return min(diff, n-diff)
+10
